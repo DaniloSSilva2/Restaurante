8da924d4b053d3cf35e64d5477b106a6d7b91b0c
DaniloSSilva2 <netunoss01234@gmail.com>
2025-06-04 20:06:12 -0300

    Update UsuarioController.java

--- src/main/java/com/example/aula/UsuarioController.java
+++ src/main/java/com/example/aula/UsuarioController.java
@@ -1,7 +1,7 @@
 package com.example.aula.controller;
 
-import com.example.aula.model.Usuario;
-import com.example.aula.service.UsuarioService;
+import com.example.aula.model.Prato;
+import com.example.aula.service.PratoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,41 +12,35 @@ import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/usuarios")
-public class UsuarioController {
+@RequestMapping("/pratos")
+public class PratoController {
 
-    private UsuarioService usuarioService;
+    private final PratoService pratoService;
 
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
+    public PratoController(PratoService pratoService) {
+        this.pratoService = pratoService;
     }
 
     @GetMapping
-    public List<Usuario> listarTodos() {
-        return usuarioService.listarTodos();
+    public List<Prato> listar() {
+        return pratoService.listarTodos();
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> salvar(@Valid @RequestBody Usuario usuario) {
-        usuarioService.salvar(usuario);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(Map.of("mensagem", "Usuário cadastrado com sucesso."));
+    public ResponseEntity<Map<String, Object>> salvar(@Valid @RequestBody Prato prato) {
+        pratoService.salvar(prato);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("mensagem", "Prato cadastrado com sucesso."));
     }
 
     @PutMapping
-    public ResponseEntity<Map<String, Object>> atualizar(@Valid @RequestBody Usuario usuario) {
-        usuarioService.atualizar(usuario);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(Map.of("mensagem", "Usuário atualizado com sucesso"));
+    public ResponseEntity<Map<String, Object>> atualizar(@Valid @RequestBody Prato prato) {
+        pratoService.atualizar(prato);
+        return ResponseEntity.ok(Map.of("mensagem", "Prato atualizado com sucesso."));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> excluir(@PathVariable Long id) {
-        usuarioService.excluir(id);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(Map.of("mensagem", "Usuário excluído com sucesso"));
+        pratoService.excluir(id);
+        return ResponseEntity.ok(Map.of("mensagem", "Prato excluído com sucesso."));
     }
 }
